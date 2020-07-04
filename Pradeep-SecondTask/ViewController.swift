403472e4d31ffd5444dead95e99e9ec50dc25ee3
iOS-Pradeep <pradeep.sagar.ios@gmail.com>
2020-07-04 22:28:18 +0530

    Initial Commit

---
+++ Pradeep-SecondTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pradeep-SecondTask
+//
+//  Created by Pradeep on 04/07/20.
+//  Copyright © 2020 Pradeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
